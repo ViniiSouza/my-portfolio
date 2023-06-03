91cc1308d2fead63b39a2fa7d8ba1017bea10021
Vinícius <vinigsouza.bnu@gmail.com>
2023-06-02 21:48:56 -0300

    feat(presentation): separate style in new file + typewriter effect in name

--- src/components/home/components/presentation/Presentation.vue
+++ src/components/home/components/presentation/Presentation.vue
@@ -1,74 +1,55 @@
 <template>
     <div id="introduction__section">
-        <div class="introduction__hello-text">Olá, eu sou o: <span class="introduction__name-text">Vinícius Gabriel de Souza</span></div>
+        <div class="introduction__hello-text">Olá, eu sou o: <div class="introduction__name-container"><span ref="name" class="introduction__name-text">Vinícius Gabriel de Souza</span></div></div>
         <div class="introduction__profession-text">FullStack Developer</div>
         <div class="introduction__description-text">Desenvolvedor de software com mais <br> de 2 anos de sólida experiência.</div>
     </div>
 </template>
 <script>
+// component style
+import './shared/style.css';
+
 export default {
     data() {
         return {
-
+            
         }
+    },
+    created() {
+      setTimeout(() => {
+        this.writeName()
+        setInterval(() => {
+          this.writeName()
+        }, 17200)
+      },200)
+    },
+    methods: {
+      async writeName() {
+        this.getNameReference().classList.add('first-animation')
+        this.getNameReference().classList.add('full-name')
+        await this.setDelay(4000)
+        this.getNameReference().classList.replace('first-animation', 'second-animation')
+        this.getNameReference().classList.replace('full-name', 'first-name')
+        await this.setDelay(5000)
+        this.getNameReference().classList.replace('second-animation', 'third-animation')
+        this.getNameReference().classList.replace('first-name', 'nick-name')
+        await this.setDelay(1100)
+        this.getNameReference().classList.replace('third-animation', 'fourth-animation')
+        await this.setDelay(1000)
+        this.getNameReference().textContent = 'Vini'
+        await this.setDelay(4000)
+        this.getNameReference().classList.replace('fourth-animation', 'fifth-animation')
+        this.getNameReference().classList.remove('nick-name')
+        await this.setDelay(2000)
+        this.getNameReference().classList.remove('fifth-animation')
+        this.getNameReference().textContent = 'Vinícius Gabriel de Souza'
+      },
+      setDelay(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+      },
+      getNameReference() {
+        return this.$refs.name
+      }
     }
 }
 </script>
-
-
-<style>
-#introduction__section {
-  font-family: "Staatliches", sans-serif;
-  text-align: center;
-  padding: 2.5rem 0;
-  display: grid;
-  grid-template-columns: 12.5% 12.5% 12.5% 12.5% 12.5% 12.5% 12.5% 12.5%;
-}
-
-.introduction__hello-text {
-    /* grid-area: hello; */
-    font-size: 3rem;
-    font-family: "Raleway", sans-serif;
-    text-align: left;
-    justify-content: start;
-    grid-column-start: 3;
-    grid-column-end: 7;
-    padding-left: 0.5rem;
-  }
-
-.introduction__name-text {
-    /* grid-area: name; */
-    font-family: "Raleway", sans-serif;
-    font-weight: bold;
-    font-size: 3rem;
-    background-color: rgb(206, 206, 206);
-    max-width: fit-content;
-    padding: 0 15px;
-}
-
-.introduction__profession-text {
-    /* grid-area: title; */
-    font-size: 15em;
-    line-height: 0.8;
-    grid-row-start: 2;
-    grid-row-end: 3;
-    grid-column-start: 3;
-    grid-column-end: 7;
-    text-align: left;
-    font-family: "Staatliches", sans-serif;
-  }
-
-  .introduction__description-text {
-    /* grid-area: description; */
-    color: rgb(100, 100, 100);
-    font-family: "Montserrat", sans-serif;
-    font-size: 1.5em;
-    line-height: 1.2;
-    grid-row-start: 4;
-    grid-column-start: 5;
-    grid-column-end: 8;
-    line-break: auto;
-    overflow-wrap: break-word;
-    text-align: left;
-  }
-</style>
