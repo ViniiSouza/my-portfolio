4340574325f98943ecc7a598e4f3c2ff91aa81e5
Vinícius <vinigsouza.bnu@gmail.com>
2023-06-04 14:14:59 -0300

    feat: implement vanilla-tilt webkit

--- src/components/knowledge/NewKnowledge.vue
+++ src/components/knowledge/NewKnowledge.vue
@@ -1,18 +1,24 @@
 <template>
   <div>
-    <div id="knowledge-section">
-      <h3 id="knowledge-title">{{ texts[language].knowledge.title }}</h3>
-      <NewStack
-        v-for="item in filteredTechs"
-        :key="item.title"
-        :imgSrc="item.imgSrc"
-        :stackTitle="item.title"
-        :stackDescription="item.description"
-      />
+    <div id="knowledge__section">
+      <h3 id="knowledge__title">{{ texts[language].knowledge.title }}</h3>
+      <div id="knowledge__stack-container">
+      <div id="knowledge__stack-grid">
+        <NewStack
+          v-for="item in filteredTechs"
+          :key="item.title"
+          :imgSrc="item.imgSrc"
+          :stackTitle="item.title"
+          :stackDescription="item.description"
+        />
+      </div>
+      </div>
     </div>
   </div>
 </template>
 <script>
+// component style
+import './shared/style.css'
 import NewStack from './shared/stack/NewStack.vue'
 import texts from '../../assets/texts/texts'
 
@@ -23,6 +29,15 @@ export default {
       selectedArea: 'all',
       filteredTechs: texts[this.language].knowledge.techs,
     }
+  },
+  mounted() {
+    const plugin = document.createElement("script");
+    plugin.setAttribute(
+      "src",
+      "./src/assets/webkits/vanilla-tilt.js"
+    );
+    plugin.async = true;
+    document.head.appendChild(plugin);
   },
   props: {
     language: {
--- src/components/knowledge/shared/stack/NewStack.vue
+++ src/components/knowledge/shared/stack/NewStack.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="stack__container">
+  <div class="stack__container" data-tilt data-tilt-max="15" data-tilt-reverse="true" data-tilt-max-glare="0.25" data-tilt-glare="true" data-tilt-scale="1.05">
     <img class="stack__image" :src="imgSrc" />
     <div class="stack__name">{{ stackTitle }}</div>
   </div>
