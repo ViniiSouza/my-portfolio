73b38c40ce4882a5e38445e04833089ab7610049
Vinícius <vinigsouza.bnu@gmail.com>
2023-06-11 19:43:44 -0300

    fix(skills): fixes and improvements in code section

--- src/components/skills/Skills.vue
+++ src/components/skills/Skills.vue
@@ -4,35 +4,24 @@
       <code id="skills__code-container">
         <code class="skills__code-header">Portfolio > skills.json</code>
         <ol class="skills__code-line-numbers">
-          <li><pre class="skills__code--bracket">skills: {</pre></li>
           <li>
-            <pre contenteditable class="skills__code--property">mostConfidentWith</pre>
+            <pre class="skills__code--bracket">{{ codeContent.name }}</pre>
           </li>
-          <li>
-            <pre contenteditable class="skills__code--string">"C Sharp", "JavaScript", "VueJS", "React", "SQL", "Bootstrap"</pre>
-          </li>
-          <li>
-            <pre class="skills__code--punctuation">],</pre>
-          </li>
-          <li>
-            <pre contenteditable class="skills__code--property">goodKnowledge</pre>
-          </li>
-          <li>
-            <pre contenteditable class="skills__code--string">"Angular", "State management libraries (Redux and Vuex)", "Docker"</pre>
-          </li>
-          <li>
-            <pre class="skills__code--punctuation">],</pre>
-          </li>
-          <li>
-            <pre contenteditable class="skills__code--property">canManageWithHelp</pre>
-          </li>
-          <li>
-            <pre contenteditable class="skills__code--string">"Java", "Python", "AWS", "TypeScript"</pre>
-          </li>
-          <li>
-            <pre class="skills__code--punctuation">]</pre>
-          </li>
-          <li><pre class="skills__code--bracket">}</pre></li>
+          <div v-for="(content, index) in codeContent.content" :key="content.name">
+            <li>
+              <pre class="skills__code--property">{{
+                content.name
+              }}</pre>
+            </li>
+            <li>
+              <pre contenteditable class="skills__code--string" @blur="updateValue($event, index)">{{
+                content.content
+              }}</pre>
+            </li>
+            <li>
+              <pre class="skills__code--punctuation">{{ content.end }}</pre>
+            </li>
+          </div>
         </ol>
       </code>
     </div>
@@ -43,4 +32,36 @@
 // component style
 import './shared/style.css'
 
+export default {
+  data() {
+    return {
+      codeContent: {
+        name: 'skills: {',
+        content: [
+          {
+            name: 'mostConfidentWith',
+            content: '"C Sharp", "JavaScript", "VueJS", "React", "SQL", "Bootstrap"',
+            end: '],',
+          },
+          {
+            name: 'goodKnowledge',
+            content:
+              '"Angular", "State management libraries (Redux and Vuex)", "Docker"',
+            end: '],',
+          },
+          {
+            name: 'canManageWithHelp',
+            content: '"Java", "Python", "AWS", "TypeScript"',
+            end: ']',
+          },
+        ],
+      },
+    }
+  },
+  methods: {
+    updateValue(event, index, name) {
+        this.codeContent.content[index].content = event.target.innerText
+    },
+  },
+}
 </script>
