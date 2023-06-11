eda18b813f5b5edd90058bec406f0fea00c7f719
Vinícius <vinigsouza.bnu@gmail.com>
2023-06-11 12:47:54 -0300

    feat(skills): style improvement

--- src/components/skills/Skills.vue
+++ src/components/skills/Skills.vue
@@ -2,24 +2,41 @@
   <div id="skills__container">
     <div id="skills__code-section">
       <code id="skills__code-container">
+        <code class="skills__code-header">Portfolio > skills.json</code>
         <ol class="skills__code-line-numbers">
-        <li><pre class="skills__code--bracket">{</pre></li>
-        <li><pre contenteditable class="skills__code--property">mostConfidentWith</pre></li>
-        <li><pre contenteditable class="skills__code--string">"C Sharp", "JavaScript", "VueJS", "React", "SQL", "Bootstrap"</pre></li>
-        <li><pre class="skills__code--punctuation">], </pre></li>
-        <li><pre contenteditable class="skills__code--property">goodKnowledge</pre></li>
-        <li><pre contenteditable class="skills__code--string">"Angular", "State management libraries (Redux and Vuex)", "Docker"</pre></li>
-        <li><pre class="skills__code--punctuation">], </pre></li>
-        <li><pre contenteditable class="skills__code--property">canManageWithHelp</pre></li>
-        <li><pre contenteditable class="skills__code--string">"Java", "Python", "AWS", "TypeScript"</pre></li>
-        <li><pre class="skills__code--punctuation">]</pre></li>
-        <li><pre class="skills__code--bracket">}</pre></li>
+          <li><pre class="skills__code--bracket">skills: {</pre></li>
+          <li>
+            <pre contenteditable class="skills__code--property">mostConfidentWith</pre>
+          </li>
+          <li>
+            <pre contenteditable class="skills__code--string">"C Sharp", "JavaScript", "VueJS", "React", "SQL", "Bootstrap"</pre>
+          </li>
+          <li>
+            <pre class="skills__code--punctuation">],</pre>
+          </li>
+          <li>
+            <pre contenteditable class="skills__code--property">goodKnowledge</pre>
+          </li>
+          <li>
+            <pre contenteditable class="skills__code--string">"Angular", "State management libraries (Redux and Vuex)", "Docker"</pre>
+          </li>
+          <li>
+            <pre class="skills__code--punctuation">],</pre>
+          </li>
+          <li>
+            <pre contenteditable class="skills__code--property">canManageWithHelp</pre>
+          </li>
+          <li>
+            <pre contenteditable class="skills__code--string">"Java", "Python", "AWS", "TypeScript"</pre>
+          </li>
+          <li>
+            <pre class="skills__code--punctuation">]</pre>
+          </li>
+          <li><pre class="skills__code--bracket">}</pre></li>
         </ol>
       </code>
     </div>
-    <div id="skills__result-container">
-      COLUNA B
-    </div>
+    <div id="skills__result-container">COLUNA B</div>
   </div>
 </template>
 <script>
