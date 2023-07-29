c03eb6ff8700f65806118e211a3c8911a5c519ae
Vinícius <vinigsouza.bnu@gmail.com>
2023-07-29 20:46:31 -0300

    fix(presentation): improves styles

--- src/components/presentation/Presentation.vue
+++ src/components/presentation/Presentation.vue
@@ -1,14 +1,20 @@
 <template>
   <div id="introduction__section">
-    <div class="introduction__hello-text">
-      {{ texts[language].presentation.introduction }}
-      <div class="introduction__name-container">
-        <span ref="name" class="introduction__name-text"
-          >Vinícius Gabriel de Souza
-        </span>
+    <div class="introduction__wrapper">
+      <div class="introduction__hello-text">
+        {{ texts[language].presentation.introduction }}
+        <div class="introduction__name-container">
+          <span ref="name" class="introduction__name-text"
+            >Vinícius Gabriel de Souza
+          </span>
+        </div>
+      </div>
+      <div class="introduction__profession-text">
+        {{ texts[language].presentation.title1 }}
+        <br />
+        {{ texts[language].presentation.title2 }}
       </div>
     </div>
-    <div class="introduction__profession-text">{{ texts[language].presentation.title }}</div>
     <div class="introduction__description-text">
       {{ texts[language].presentation.description1 }} <br />
       {{ texts[language].presentation.description2 }}
@@ -17,7 +23,7 @@
 </template>
 <script>
 // component style
-import "./shared/style.css"
+import './shared/style.css'
 import texts from '../../assets/texts/texts'
 
 export default {
@@ -42,43 +48,57 @@ export default {
   },
   methods: {
     async writeName() {
-      this.getNameReference().classList.add("introduction__name-animation--first")
-      this.getNameReference().classList.add("introduction__name-text--full-name")
+      this.getNameReference().classList.add(
+        'introduction__name-animation--first'
+      )
+      this.getNameReference().classList.add(
+        'introduction__name-text--full-name'
+      )
       await this.setDelay(4000)
       this.getNameReference().classList.replace(
-        "introduction__name-animation--first",
-        "introduction__name-animation--second"
+        'introduction__name-animation--first',
+        'introduction__name-animation--second'
       )
-      this.getNameReference().classList.replace("introduction__name-text--full-name", "introduction__name-text--first-name")
+      this.getNameReference().classList.replace(
+        'introduction__name-text--full-name',
+        'introduction__name-text--first-name'
+      )
       await this.setDelay(5000)
       this.getNameReference().classList.replace(
-        "introduction__name-animation--second",
-        "introduction__name-animation--third"
+        'introduction__name-animation--second',
+        'introduction__name-animation--third'
       )
-      this.getNameReference().classList.replace("introduction__name-text--first-name", "introduction__name-text--nick-name")
+      this.getNameReference().classList.replace(
+        'introduction__name-text--first-name',
+        'introduction__name-text--nick-name'
+      )
       await this.setDelay(1100)
       this.getNameReference().classList.replace(
-        "introduction__name-animation--third",
-        "introduction__name-animation--fourth"
+        'introduction__name-animation--third',
+        'introduction__name-animation--fourth'
       )
       await this.setDelay(1000)
-      this.getNameReference().textContent = "Vini"
+      this.getNameReference().textContent = 'Vini'
       await this.setDelay(4000)
       this.getNameReference().classList.replace(
-        "introduction__name-animation--fourth",
-        "introduction__name-animation--fifth"
+        'introduction__name-animation--fourth',
+        'introduction__name-animation--fifth'
       )
-      this.getNameReference().classList.remove("introduction__name-text--nick-name")
+      this.getNameReference().classList.remove(
+        'introduction__name-text--nick-name'
+      )
       await this.setDelay(2000)
-      this.getNameReference().classList.remove("introduction__name-animation--fifth")
-      this.getNameReference().textContent = "Vinícius Gabriel de Souza"
+      this.getNameReference().classList.remove(
+        'introduction__name-animation--fifth'
+      )
+      this.getNameReference().textContent = 'Vinícius Gabriel de Souza'
     },
     setDelay(ms) {
       return new Promise((resolve) => setTimeout(resolve, ms))
     },
     getNameReference() {
       return this.$refs.name
-    }
+    },
   },
 }
 </script>
