75496d9ff6db95406fbf0acd019727b1fc9ed999
Vinícius <vinigsouza.bnu@gmail.com>
2023-07-29 21:23:12 -0300

    fix(about): improves styles

--- src/components/about/About.vue
+++ src/components/about/About.vue
@@ -1,5 +1,4 @@
 <template>
-  <div id="home-content">
     <div id="intro-section">
       <h3 id="intro-title">{{ texts[language].home.introTitle }}</h3>
       <p id="intro-text">
@@ -12,10 +11,10 @@
         >.
       </p>
     </div>
-  </div>
 </template>
 
 <script>
+import './shared/styles.css'
 import texts from '../../assets/texts/texts'
 
 export default {
@@ -32,24 +31,3 @@ export default {
   },
 }
 </script>
-<style>
-#home-content {
-  font-family: Calibri, sans-serif;
-}
-#intro-section {
-  background-color: var(--primary);
-  color: var(--white);
-  padding: 10vh 0 10vh 0;
-}
-#intro-title {
-  font-family: var(--title-font);
-  font-size: 4em;
-  text-align: center;
-}
-#intro-text {
-  font-family: 'Montserrat', sans-serif;
-  text-align: justify;
-  margin: 0 8vw 0 8vw;
-  font-size: 1.2rem;
-}
-</style>
