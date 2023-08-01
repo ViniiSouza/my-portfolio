127d6626e9425dbb7630db7b11a090bd3192091a
Vinícius <vinigsouza.bnu@gmail.com>
2023-07-31 22:02:44 -0300

    feat(contact): copy email improvement

--- src/components/contact/Contact.vue
+++ src/components/contact/Contact.vue
@@ -3,15 +3,22 @@
     <h3 id="contact__title">{{ texts[language].contact.title }}</h3>
     <div id="contact__email">
       <p id="contact__email-icon"><i class="bi bi-envelope-paper"></i></p>
-      <h4>{{ texts[language].contact.email.description }}</h4>
+      <h4 class="contact__item-title">
+        {{ texts[language].contact.email.description }}
+      </h4>
       <div id="contact__email__input">
         <div id="contact__email__input__wrapper">
-
           <span>{{ email }}</span>
-          <button @click="copyEmail"><i :class="copying ? 'bi bi-check' : 'bi bi-clipboard'"></i></button>
+          <button @click="copyEmail">
+            <i :class="copying ? 'bi bi-check' : 'bi bi-clipboard'"></i>
+          </button>
         </div>
       </div>
-      <span id="contact__email-feedback" :class="copying ? 'feedback-animation' : ''">{{ texts[language].contact.email.feedback }}</span>
+      <span
+        id="contact__email-feedback"
+        :class="copying ? 'feedback-animation' : ''"
+        >{{ texts[language].contact.email.feedback }}</span
+      >
     </div>
   </div>
 </template>
@@ -31,17 +38,51 @@ export default {
   props: {
     language: {
       type: String,
-      default: 'pt'
-    }
+      default: 'pt',
+    },
   },
   methods: {
     copyEmail() {
-      navigator.clipboard.writeText(this.email)
+      if (!navigator.clipboard) {
+        this.copyIos()
+      } else {
+        navigator.clipboard.writeText(this.email)
+      }
       this.copying = true
       setTimeout(() => {
         this.copying = false
       }, 2000)
-    }
-  }
+    },
+    copyIos() {
+      // from https://gist.github.com/wilsonpage/6f15d9b173584195eaa5dee42215bd81
+      const isIos = navigator.userAgent.match(/ipad|iphone/i)
+      const textarea = document.createElement('textarea')
+
+      // create textarea
+      textarea.value = this.email
+
+      // ios will zoom in on the input if the font-size is < 16px
+      textarea.style.fontSize = '20px'
+      document.body.appendChild(textarea)
+      // select text
+      if (isIos) {
+        const range = document.createRange()
+        range.selectNodeContents(textarea)
+
+        const selection = window.getSelection()
+        selection.removeAllRanges()
+        selection.addRange(range)
+        textarea.setSelectionRange(0, 999999)
+      } else {
+        textarea.select()
+      }
+
+      // copy selection
+      document.execCommand('copy')
+
+      // cleanup
+      document.body.removeChild(textarea)
+    },
+  },
 }
 </script>
