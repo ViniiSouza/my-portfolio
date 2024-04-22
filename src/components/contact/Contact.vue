e8318166af760e0da42c7ad30956b8745f33574c
Vinícius Souza <vinigsouza.bnu@gmail.com>
2024-04-22 00:55:09 -0300

    fix: links to resume

--- src/components/contact/Contact.vue
+++ src/components/contact/Contact.vue
@@ -28,7 +28,11 @@
       <div class="contact__item__input">
         <div class="contact__item__input__wrapper">
           <span>{{ number }}</span>
-          <a class="contact__item__action" target="_blank" href="https://wa.me/5547996458019">
+          <a
+            class="contact__item__action"
+            target="_blank"
+            href="https://wa.me/5547996458019"
+          >
             <i class="bi bi-send"></i>
           </a>
         </div>
@@ -42,7 +46,15 @@
       <div class="contact__item__input">
         <div class="contact__item__input__wrapper">
           <span>Download:</span>
-          <a class="contact__item__action" target="_blank" href="https://wa.me/5547996458019">
+          <a
+            class="contact__item__action"
+            target="_blank"
+            :href="
+              language == 'pt'
+                ? 'https://drive.google.com/file/d/17qtNu3I_ipZLUlbefznrRozJGI4a2Nwv/view?usp=sharing'
+                : 'https://drive.google.com/file/d/1XOa4Or4heizqa4aWNHwvkqRCGL4BTOwj/view?usp=sharing'
+            "
+          >
             <i class="bi bi-cloud-download"></i>
           </a>
         </div>
@@ -51,8 +63,8 @@
   </section>
 </template>
 <script>
-import './shared/styles.css'
-import texts from '../../assets/texts/texts.json'
+import './shared/styles.css';
+import texts from '../../assets/texts/texts.json';
 
 export default {
   data() {
@@ -62,7 +74,7 @@ export default {
       emailIcon: 'bi bi-clipboard',
       number: '+55 47 99645-8019',
       copying: false,
-    }
+    };
   },
   props: {
     language: {
@@ -73,45 +85,45 @@ export default {
   methods: {
     copyEmail() {
       if (!navigator.clipboard) {
-        this.copyIos()
+        this.copyIos();
       } else {
-        navigator.clipboard.writeText(this.email)
+        navigator.clipboard.writeText(this.email);
       }
-      this.copying = true
+      this.copying = true;
       setTimeout(() => {
-        this.copying = false
-      }, 2000)
+        this.copying = false;
+      }, 2000);
     },
     copyIos() {
       // from https://gist.github.com/wilsonpage/6f15d9b173584195eaa5dee42215bd81
-      const isIos = navigator.userAgent.match(/ipad|iphone/i)
-      const textarea = document.createElement('textarea')
+      const isIos = navigator.userAgent.match(/ipad|iphone/i);
+      const textarea = document.createElement('textarea');
 
       // create textarea
-      textarea.value = this.email
+      textarea.value = this.email;
 
       // ios will zoom in on the input if the font-size is < 16px
-      textarea.style.fontSize = '20px'
-      document.body.appendChild(textarea)
+      textarea.style.fontSize = '20px';
+      document.body.appendChild(textarea);
       // select text
       if (isIos) {
-        const range = document.createRange()
-        range.selectNodeContents(textarea)
+        const range = document.createRange();
+        range.selectNodeContents(textarea);
 
-        const selection = window.getSelection()
-        selection.removeAllRanges()
-        selection.addRange(range)
-        textarea.setSelectionRange(0, 999999)
+        const selection = window.getSelection();
+        selection.removeAllRanges();
+        selection.addRange(range);
+        textarea.setSelectionRange(0, 999999);
       } else {
-        textarea.select()
+        textarea.select();
       }
 
       // copy selection
-      document.execCommand('copy')
+      document.execCommand('copy');
 
       // cleanup
-      document.body.removeChild(textarea)
+      document.body.removeChild(textarea);
     },
   },
-}
+};
 </script>
